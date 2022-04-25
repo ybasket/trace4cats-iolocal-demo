3deb2dfa2d987d7c6f3fa221c82dfcc8c3f9953e
Yannick Heiber <yannick.heiber@mobimeo.com>
2022-04-25 18:33:22 +0200

    Update to newer state of IOLocal PR

--- build.sbt
+++ build.sbt
@@ -8,10 +8,12 @@ lazy val root = (project in file("."))
     libraryDependencies ++= List(
       "io.janstenpickle" %% "trace4cats-http4s-server" % "0.13.1",
       "io.janstenpickle" %% "trace4cats-jaeger-thrift-exporter" % "0.13.1",
-      "io.janstenpickle" %% "trace4cats-base" % "0.13.1+22-eca87239",
-      "io.janstenpickle" %% "trace4cats-base" % "0.13.1+22-eca87239",
-      "io.janstenpickle" %% "trace4cats-cats-effect-io" % "0.13.1+22-eca87239",
-      "io.janstenpickle" %% "trace4cats-model" % "0.13.1+22-eca87239",
+      "io.janstenpickle" %% "trace4cats-base" % "0.13.1+28-784b7b3c",
+      "io.janstenpickle" %% "trace4cats-base-io" % "0.13.1+28-784b7b3c",
+      "io.janstenpickle" %% "trace4cats-core" % "0.13.1+28-784b7b3c",
+      "io.janstenpickle" %% "trace4cats-inject" % "0.13.1+28-784b7b3c",
+      "io.janstenpickle" %% "trace4cats-inject-io" % "0.13.1+28-784b7b3c",
+      "io.janstenpickle" %% "trace4cats-model" % "0.13.1+28-784b7b3c",
       "org.http4s" %% "http4s-ember-server" % "0.23.11",
       "org.http4s" %% "http4s-circe" % "0.23.11",
       "org.http4s" %% "http4s-dsl" % "0.23.11"
--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -1,19 +1,21 @@
 package demo
 
+import cats.effect.std.Random
 import cats.effect.{ExitCode, IO, IOApp, IOLocal, Resource}
 import cats.syntax.all._
 import io.janstenpickle.trace4cats.Span
 import io.janstenpickle.trace4cats.base.context.Provide
-import io.janstenpickle.trace4cats.cats.effect._
+import io.janstenpickle.trace4cats.inject.io._
 import io.janstenpickle.trace4cats.inject.{EntryPoint, Trace}
 import io.janstenpickle.trace4cats.jaeger.JaegerSpanCompleter
 import io.janstenpickle.trace4cats.kernel.SpanSampler
 import io.janstenpickle.trace4cats.model.TraceProcess
 
 object Main extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] =
+  // custom implicit random protects against https://github.com/trace4cats/trace4cats/issues/740
+  override def run(args: List[String]): IO[ExitCode] = Random.scalaUtilRandom[IO].flatMap { implicit rand =>
     JaegerSpanCompleter[IO](TraceProcess("fibonacci")).use { spanCompleter =>
-      Resource.eval(DischargeSpan[IO](SpanSampler.always[IO], spanCompleter)).use { dischargeSpan =>
+      Resource.eval(Span.discharge[IO](SpanSampler.always[IO], spanCompleter)).use { dischargeSpan =>
         IOLocal[Span[IO]](dischargeSpan).flatMap { ioLocal =>
           implicit val trace: Trace[IO] = ioLocalTrace(ioLocal)
           implicit val provide: Provide[IO, IO, Span[IO]] = ioLocalProvide(ioLocal)
@@ -23,4 +25,5 @@ object Main extends IOApp {
         }
       }
     }
+  }
 }
